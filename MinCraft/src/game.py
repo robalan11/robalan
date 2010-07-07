d71dafadf7c0081dfc0553476a07d3ca90e14bff
robalan <robalan@f07626f4-735e-b920-ae0c-283727fffbca>
2010-07-07 06:30:40 +0000

    [Mincraft] Can scroll around levels.  Fixed resolution of tiles, added character sprite.

--- MinCraft/src/game.py
+++ MinCraft/src/game.py
@@ -1,8 +1,12 @@
 import sys, pygame
+import world
 
 class Game(object):
     def __init__(self):        
         self._clock = pygame.time.Clock()
+        self._world = world.World()
+        self._pos = [128, 64, 0, 0]
+    
     def run(self, screen):
         while True:
             # Get pressed keys
@@ -11,13 +15,39 @@ class Game(object):
             if ((keys[pygame.K_RALT] or keys[pygame.K_LALT]) and keys[pygame.K_F4]) or keys[pygame.K_ESCAPE]:
                 sys.exit()
             
+            # Move (temp)
+            if keys[pygame.K_w]:
+                self._pos[3] -= 16
+            if keys[pygame.K_a]:
+                self._pos[2] -= 16
+            if keys[pygame.K_s]:
+                self._pos[3] += 16
+            if keys[pygame.K_d]:
+                self._pos[2] += 16
+            
+            if self._pos[2] >= 32:
+                self._pos[0] += 1
+                self._pos[2] -= 32
+            if self._pos[2] < 0:
+                self._pos[0] -= 1
+                self._pos[2] += 32
+            if self._pos[3] >= 32:
+                self._pos[1] += 1
+                self._pos[3] -= 32
+            if self._pos[3] < 0:
+                self._pos[1] -= 1
+                self._pos[3] += 32
+            
             # Check events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
             
-            # Display things
+            # Update the world
+            self._world.update(self._pos)
             
+            # Display things
+            self._world.draw(screen, self._pos)
             
             # Flip buffers and tick the clock
             pygame.display.flip()
